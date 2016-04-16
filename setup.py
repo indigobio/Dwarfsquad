93e46a5291322c4b4742e7953802eb78cb5d4b05
Kenneth Tussey <ktussey@indigobio.com>
2016-04-16 01:31:33 -0400

    Fix some requirements.

--- setup.py
+++ setup.py
@@ -13,4 +13,7 @@ setup(name='dwarfsquad',
       author_email='whereskenneth@gmail.com',
       scripts = ['dwarfsquad/dwarfsquad'],
       packages= find_packages(),
-      install_requires=['pymongo', 'behave>=1.2', 'requests==2.1.0', 'pyaml', 'tqdm>=1.0', 'requests_cache>=0.4.4'])
+      install_requires=[
+          'pymongo', 'behave>=1.2', 'requests==2.1.0', 'pyaml',
+          'tqdm>=1.0', 'requests_cache>=0.4.4','openpyxl==2.3.5'
+      ])
